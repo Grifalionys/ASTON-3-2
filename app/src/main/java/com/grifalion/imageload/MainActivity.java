0e1cf2c069d9847c07c819709c527880ce7642a8
Nickolay Makushevskii <creywer@gmail.com>
2023-03-26 19:27:19 +0300

    Без кнопки отправить

--- app/src/main/java/com/grifalion/imageload/MainActivity.java
+++ app/src/main/java/com/grifalion/imageload/MainActivity.java
@@ -3,33 +3,24 @@ package com.grifalion.imageload;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.load.model.stream.MediaStoreImageThumbLoader;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
     private ImageView imageView;
     private EditText edSearch;
     private Button btnSearch;
@@ -41,25 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageView);
         edSearch = findViewById(R.id.edSearch);
-        btnSearch = findViewById(R.id.btnSearch);
+        edSearch.addTextChangedListener( this);
     }
 
-    public void onClickSearch(View view) {
-            imageView.setVisibility(View.VISIBLE);
-            Glide.with(this)
-                    .load(edSearch.getText().toString())
-                    .listener(new RequestListener<Drawable>() {
-                        @Override
-                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                            Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
-                            return false;
-                        }
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
 
-                        @Override
-                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                            return false;
-                        }
-                    })
-                    .into(imageView);
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        Glide.with(this)
+                .load(charSequence.toString())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .into(imageView);
     }
+
+    @Override
+    public void afterTextChanged(Editable editable) { }
 }
